72c65a02103d0aafb13ba67b0cea1579c6c5ffd5
Joe Dallago <jd.dallago@gmail.com>
2012-10-17 19:38:01 +0000

    Added the initial file structure.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#clusterflunk/setup.py
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.md')).read()
+
+requires = [
+    'colander'
+]
+
+tests_require = requires + [
+    'nose',
+    'coverage',
+    'mock'
+]
+
+setup(name='soap',
+      version='0.0.1',
+      description='Serialization/deserialization library that supports relationships.',
+      long_description=README + '\n\n' + CHANGES,
+      install_requires=requires,
+      tests_require=tests_require,
+      packages=['soap'],
+      test_suite='soap.tests'
+)
