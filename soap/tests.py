fbadc36de53291085cd8acba1482946868c012f8
Joe Dallago <jd.dallago@gmail.com>
2012-11-01 01:13:54 +0000

    Committing before I remove most of the starter code.

--- soap/tests.py
+++ soap/tests.py
@@ -15,8 +15,23 @@ class A(MappingSchema):
 class B(MappingSchema):
     c = SchemaNode(Int())
     d = SchemaNode(Int())
-    a = relationship('A', backref='b_dealt', one_to_one=True)
+    a = relationship('A', uselist=False)
+
 
 a_deal = A()
-output = a_deal.deserialize({'a': 'blah', 'b': [{'c': 1, 'd': 1, 'a': {'a': 'asdf', 'b': []}}, {'c': 1, 'd': 2, 'a': {'a': 'haha', 'b': []}}]})
+output = a_deal.deserialize({'a': 'blah',
+                             'b': [{
+                                'c': 1,
+                                'd': 1,
+                                'a': {
+                                    'a': 'asdf'
+                                }
+                             },
+                             {
+                                'c': 1,
+                                'd': 2,
+                                'a': {
+                                    'a': 'haha'
+                                }
+                             }]})
 print(output)
