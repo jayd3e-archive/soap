fdbc95290c6b5034e401ecd6d9b459f834fb0efa
Joe Dallago <jd.dallago@gmail.com>
2012-11-04 05:23:28 +0000

    Worked out some kinks that were found when testing against clusterflunk's schemas.

--- soap/__init__.py
+++ soap/__init__.py
@@ -1,3 +1,6 @@
+falsey = ['', {}, []]
+
+
 class Invalid(Exception):
     def __init__(self, msg, node, model):
         self.msg = msg
@@ -7,6 +10,10 @@ class Invalid(Exception):
     def __str__(self):
         return 'Invalid(\'%s\', %s, %s)' % (self.msg, self.model, self.node)
 
+
+#
+# Types
+#
 
 class Int(object):
     def deserialize(self, json, node, model):
@@ -32,7 +39,7 @@ class Mapping(object):
         for child in node.children:
             value = validated.get(child.name, None)
             if not value is None:
-                deserialized[child.name] = child.deserialize(value, node, model)
+                deserialized[child.name] = child.deserialize(value, model=model)
             elif not child.missing is None:
                 deserialized[child.name] = child.missing
             else:
@@ -55,7 +62,7 @@ class Sequence(object):
         for value in validated:
             child = node.children[0]
             if child:
-                deserialized.append(child.deserialize(value, node, model))
+                deserialized.append(child.deserialize(value, model=model))
 
         return deserialized
 
@@ -75,23 +82,49 @@ class Relationship(object):
 
     def deserialize(self, json, node, model):
         inst = model._models[self.model_name]
-        inst = inst if isinstance(inst, SchemaModel) else inst()
+        inst = inst if isinstance(inst, SchemaModel) else inst(name=node.name,
+                                                               missing=node.missing)
 
         if self.uselist:
             schema_model = SchemaNode(Sequence(),
                                       inst,
-                                      name=node.name)
+                                      name=node.name,
+                                      missing=node.missing)
         else:
             schema_model = inst
 
-        return schema_model.deserialize(json, node, model)
+        return schema_model.deserialize(json, model=model)
 
+
+#
+# Validators
+#
+
+class Length(object):
+    def __init__(self, _min=None, _max=None):
+        self.min = _min
+        self.max = _max
+
+    def __call__(self, value, node, model):
+        if self.min is not None:
+            if len(value) < self.min:
+                raise Invalid('Shorter than minimum length %s' % self.min, node, model)
+
+        if self.max is not None:
+            if len(value) > self.max:
+                raise Invalid('Longer than maximum length %s' % self.max, node, model)
+
+
+#
+# Core
+#
 
 class SchemaNode(object):
     _type = None
     children = None
     name = ''
     missing = None
+    validator = None
 
     def __init__(self, *args, **kwargs):
         self.children = []
@@ -102,12 +135,29 @@ class SchemaNode(object):
 
         self.name = kwargs.get('name', '')
         self.missing = kwargs.get('missing', None)
+        self.validator = kwargs.get('validator', None)
+
+    @property
+    def required(self):
+        return self.missing is None
 
     def deserialize(self, json, node=None, model=None):
         node = node if node else self
         model = model if model else self
 
-        deserialized = self._type.deserialize(json, self, model)
+        deserialized = self._type.deserialize(json, node, model)
+
+        # preparers logic
+
+        if json in falsey and node.required:
+            raise Invalid('%s is required.' % node.name, node, model)
+
+        if self.validator and type(self.validator) is list:
+            for validator in self.validator:
+                validator(json, node, model)
+        elif self.validator:
+            self.validator(json, node, model)
+
         return deserialized
 
     def get(self, name, default=None):
@@ -149,6 +199,8 @@ class SchemaModel(SchemaNode):
 
             self._type = args[1]
             self.children = list(args[2:])
+
+        self.__dict__.update(kwargs)
 
     def validate(self, json):
         return self.deserialize(json)
